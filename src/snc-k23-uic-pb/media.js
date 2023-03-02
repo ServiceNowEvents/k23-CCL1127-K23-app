68fb2bd07babbce9d23e53ae5d2748b03dd59ed6
jon.lind <jon.lind@servicenow.com>
2023-03-02 16:23:02 -0700

    Refactored media.js to match the main photobooth-uic-camera project.

--- src/snc-k23-uic-pb/media.js
+++ src/snc-k23-uic-pb/media.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 export function switchMediaDevice({
 	video,
 	cameraDeviceId,
@@ -5,6 +6,10 @@ export function switchMediaDevice({
 	updateState
 }) {
 	console.log("SWITCH MEDIA DEVICE", { cameraDeviceId, enabled });
+=======
+export function selectMediaDevice({ video, cameraDeviceId = "", enabled }) {
+	console.log("SELECT MEDIA DEVICE", { cameraDeviceId, enabled });
+>>>>>>> ad116b9 (Refactored media.js to match the main photobooth-uic-camera project.)
 	// Get access to the camera!
 	navigator.mediaDevices
 		.getUserMedia({ video: { deviceId: cameraDeviceId } })
@@ -66,6 +71,7 @@ export function getConnectedDevices({
     });
 }
 
+<<<<<<< HEAD
 export function drawImage(
 	pos,
 	{
@@ -79,6 +85,30 @@ export function drawImage(
 ) {
 	const hWidth = width / 2;
 	const hHeight = height / 2;
+=======
+export function initializeCanvas({
+	context,
+	imageSize = { width: 800, height: 600 },
+	fillStyle,
+}) {
+	const { canvas } = context;
+	console.log("Initialize Canvas", canvas, imageSize, fillStyle);
+	// Add room for gaps above, between and below images
+	canvas.width = imageSize.width;
+	canvas.height = imageSize.height;
+
+	context.fillStyle = fillStyle;
+	context.fillRect(0, 0, canvas.width, canvas.height);
+}
+
+export function drawImage({ pos, context, video, gap = 10, chin = 0 }) {
+	const {
+		canvas: { width, height },
+	} = context;
+	// Make the shots slightly smaller to accomodate the gap/chin
+	const hWidth = (width / 2) - (gap * 3) / 2;
+	const hHeight = ((height - chin) / 2) - (gap * 3) / 2;
+>>>>>>> ad116b9 (Refactored media.js to match the main photobooth-uic-camera project.)
 
 	// Define where the first, second, third and fourth images appear
 	// in the grid
@@ -93,3 +123,59 @@ export function drawImage(
 
 	context.drawImage(video, x, y, hWidth, hHeight);
 }
+<<<<<<< HEAD
+=======
+
+// Passing in "state" instead of destructuring it in place
+// becuase drawImage needs a lot of values from state
+// and I don't want to have to call them out twice
+export function snap({ state, updateState, onIndividualSnap }) {
+	const {
+		video,
+		context,
+		shutterSound,
+		properties: {
+			countdownDurationSeconds,
+			pauseDurationSeconds = 1,
+			pauseDurationMilliseconds = pauseDurationSeconds * 1000,
+			gap,
+			chin
+		},
+	} = state;
+
+	let pos = 1;
+
+	if (countdownDurationSeconds > 0) {
+		updateState({ snapState: "countdown" });
+	}
+
+	return new Promise((resolve) => {
+		const _snap = () => {
+			console.log("_snap", pos, context);
+			updateState({ snapState: "snapping" });
+
+			drawImage({ pos, context, video, gap, chin });
+
+			if(shutterSound){
+				shutterSound.play();
+			}
+
+			if(onIndividualSnap){
+				const imageData = context.canvas.toDataURL("image/jpeg");
+				onIndividualSnap({imageData});
+			}
+
+			if (pos < 4) {
+				pos++;
+				setTimeout(_snap, pauseDurationMilliseconds);
+			} else {
+				updateState({ snapState: "preview" });
+
+				resolve({context});
+			}
+		};
+
+		setTimeout(_snap, countdownDurationSeconds * 1000);
+	});
+}
+>>>>>>> ad116b9 (Refactored media.js to match the main photobooth-uic-camera project.)

0981274ada9ca6fbdeb47086871b47bfa13fbbaa
jon.lind <jon.lind@servicenow.com>
2023-03-10 10:18:22 -0700

    Refactoring media.js to include single snaps

--- src/snc-k23-uic-pb/media.js
+++ src/snc-k23-uic-pb/media.js
@@ -24,7 +24,8 @@ export function selectMediaDevice({ video, cameraDeviceId = "", enabled }) {
 }
 
 export function toggleTracks({ video: { srcObject: stream }, enabled }) {
-	if (stream) {
+	// Don't use falsy to enable tracks, actual true or false only
+	if (stream && (enabled === true || enabled === false)) {
 		stream.getTracks().forEach((track) => (track.enabled = enabled));
 	}
 }
@@ -62,30 +63,27 @@ export function getConnectedDevices({
 			resolver(updatedCameras);
 		});
 	});
-}
+};
 
-export function initializeCanvas({
-	context,
-	imageSize = { width: 800, height: 600 },
-	fillStyle,
+function initializeCanvas(context, {
+	imageSize : { width = 800, height = 600 },
+	fillStyle = null,
 }) {
-	const { canvas } = context;
-	console.log("Initialize Canvas", canvas, imageSize, fillStyle);
-	// Add room for gaps above, between and below images
-	canvas.width = imageSize.width;
-	canvas.height = imageSize.height;
+	context.canvas.width = width;
+	context.canvas.height = height;
 
-	context.fillStyle = fillStyle;
-	context.fillRect(0, 0, canvas.width, canvas.height);
+	context.fillStyle = fillStyle || context.fillStyle;
+	context.fillRect(0, 0, width, height);
 }
 
-export function drawImage({ pos, context, video, gap = 10, chin = 0 }) {
-	const {
-		canvas: { width, height },
-	} = context;
-	// Make the shots slightly smaller to accomodate the gap/chin
-	const hWidth = (width / 2) - (gap * 3) / 2;
-	const hHeight = ((height - chin) / 2) - (gap * 3) / 2;
+function drawToSnapImage({ pos, context, video, gap = 10, chin = 0 }) {
+	console.log("drawToSnapImage", {context})
+
+	const {width, height} = context.canvas;
+	
+	// Subtract out gap and chin to make image size proportional
+	const hWidth = ((width - (gap * 3)) / 2);
+	const hHeight = ((height - (gap * 3) - chin) / 2);
 
 	// Define where the first, second, third and fourth images appear
 	// in the grid, taking into account offsets from the gap
@@ -98,13 +96,19 @@ export function drawImage({ pos, context, video, gap = 10, chin = 0 }) {
 
 	const { x, y } = posMap[pos];
 
-	context.drawImage(video, x, y, hWidth, hHeight);
+	drawImage(context, video, {x, y, width : hWidth, height : hHeight})
+
+	return { context };
+}
+
+function drawImage(context, video, {x = 0, y = 0, width, height}){
+	context.drawImage(video, x, y, width, height);
 }
 
 // Passing in "state" instead of destructuring it in place
 // becuase drawImage needs a lot of values from state
 // and I don't want to have to call them out twice
-export function snap({ state, updateState, onIndividualSnap }) {
+export function snap({ state, updateState }) {
 	const {
 		video,
 		context,
@@ -113,40 +117,58 @@ export function snap({ state, updateState, onIndividualSnap }) {
 			countdownDurationSeconds,
 			pauseDurationSeconds = 1,
 			pauseDurationMilliseconds = pauseDurationSeconds * 1000,
-			gap,
-			chin
+			gap = 0,
+			chin = 0,
+			imageSize = {width : 800, height : 600},
+			fillStyle
 		},
 	} = state;
 
+	const NUMBER_OF_SNAPS = 4;
+	const singleSnapContexts = [];
 	let pos = 1;
+
+	const mainCanvasSize = { width : (imageSize.width + (gap * 3)), height : (imageSize.height + (gap * 3) + chin)};
+	initializeCanvas(context, {imageSize : mainCanvasSize, fillStyle});
 
 	if (countdownDurationSeconds > 0) {
 		updateState({ snapState: "countdown" });
 	}
 
+	// Initialize the snap contexts to return for the single snaps in advance
+	// Man, I haven't done a for loop like this in years!
+	for(let i=0; i<NUMBER_OF_SNAPS; i++){
+		const singleSnapContext = context.canvas.ownerDocument.createElement("canvas").getContext("2d");
+		initializeCanvas(singleSnapContext, {imageSize});
+		singleSnapContexts.push(singleSnapContext);
+	}
+
+
 	return new Promise((resolve) => {
+	
 		const _snap = () => {
 			console.log("_snap", pos, context);
 			updateState({ snapState: "snapping" });
 
-			drawImage({ pos, context, video, gap, chin });
+
+			// Draw the primary 2x2 result to the main context
+			drawToSnapImage({ pos, context, video, gap, chin });
+		
+			const singleSnapContext = singleSnapContexts[pos-1];
+			// Draw the individual image full-sized
+			drawImage(singleSnapContext, video, imageSize);
 
 			if(shutterSound){
 				shutterSound.play();
 			}
 
-			if(onIndividualSnap){
-				const imageData = context.canvas.toDataURL("image/jpeg");
-				onIndividualSnap({imageData});
-			}
-
-			if (pos < 4) {
+			if (pos < NUMBER_OF_SNAPS) {
 				pos++;
 				setTimeout(_snap, pauseDurationMilliseconds);
 			} else {
 				updateState({ snapState: "preview" });
 
-				resolve({context});
+				resolve({context, singleSnapContexts});
 			}
 		};
 
